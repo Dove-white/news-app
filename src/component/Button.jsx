b9dcbfd04e4d14401de150ede2580b07b48ebbc2
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-01-28 20:24:58 +0000

    Code format

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Button = ({ name, className }) => {
-  return (
-    <button
-      className={className}
-    >
-      {name}
-    </button>
-  );
+  return <button className={className}>{name}</button>;
 };
 
 export default Button;
