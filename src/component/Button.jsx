fc2708348a1ec8a35b375d8724fad684e395eb9a
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-01-28 19:11:47 +0000

    Add button.

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Button = ({ name }) => {
-  return <button>{name}</button>;
+const Button = ({ name, className }) => {
+  return (
+    <button
+      className={className}
+    >
+      {name}
+    </button>
+  );
 };
 
 export default Button;
