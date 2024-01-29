53506d7c12be10fd9d351d8069be5db9f219944b
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-01-29 11:59:34 +0000

    Get API and loop through with map method

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleNews from "./SingleNews";
 
 const News = () => {
+  const url = import.meta.env.VITE_REACT_APP_BASE_URL;
+
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${url}/api/v1/news`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
     <>
       <main>
         <h2 className="text-3xl font-bold text-center">News</h2>
         <div className="flex flex-wrap justify-between max-md:justify-center">
-          <SingleNews />
-          <SingleNews />
-          <SingleNews />
-          <SingleNews />
+          {data.map((item) => (
+            <SingleNews
+              key={item.id}
+              title={item.title}
+              content={item.content}
+              imageUrl={item.image}
+            />
+          ))}
         </div>
       </main>
     </>
