cb841f04b208bccef14179c987db291878e80b3f
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-01-31 13:29:37 +0000

    Add confirm function to delete

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -2,26 +2,39 @@ import React, { useEffect, useState } from "react";
 import SingleNews from "./SingleNews";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const News = () => {
-  const notify = () =>
-    toast.success("News deleted", {
-      position: "top-center",
-      autoClose: 2000,
-      theme: "light",
-    });
+  const [success, setSuccess] = useState(null);
+  console.log("success", success);
 
   const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
 
   let [data, setData] = useState([]);
 
-  useEffect(() => {
+  const fetchNews = () => {
     fetch(`${baseUrl}/api/v1/news`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
       });
+  };
+
+  useEffect(() => {
+    fetchNews();
   }, []);
+
+  useEffect(() => {
+    if (success == true) {
+      toast.success("News deleted", {
+        position: "top-center",
+        autoClose: 2000,
+        theme: "light",
+      });
+      fetchNews();
+    }
+  }, [success]);
 
   // Delete News
   const deleteNews = async (id) => {
@@ -32,7 +45,11 @@ const News = () => {
           "Content-Type": "application/json; charset=UTF-8",
         },
       });
+      if (response.ok) {
+        setSuccess(response.ok);
+      }
       if (!response.ok) {
+        toast.error(response.status)
         throw new Error(`http error status: ${response.status}`);
       }
     } catch (error) {
@@ -40,10 +57,20 @@ const News = () => {
     }
   };
 
-  const handleDelete = (event) => {
-    deleteNews(event);
-    console.log(event);
-    notify();
+  const handleDelete = (id) => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: '',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => deleteNews(id)
+        },
+        {
+          label: 'No',
+        }
+      ]
+    });
   };
 
   return (
