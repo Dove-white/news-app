5bdca64c5e00e4dc376d0b39c8d3c807fec58eff
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-02-12 11:08:01 +0000

    Fix: get data and map it to it's elements.

--- src/pages/news/SingleNews.jsx
+++ src/pages/news/SingleNews.jsx
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../component/ui/Button";
-import { Link } from "react-router-dom";
+import UpdateNews from "./UpdateNews";
 
-const SingleNews = ({ title, content, imageUrl, deleteNews, updateNews, id }) => {
+const SingleNews = ({ data, handleDelete, setUpdateSuccessful }) => {
+  let [editForm, setEditForm] = useState(false);
+  let [items, setItems] = useState({});
+
   return (
     <>
-      <article className="w-[300px] flex flex-col gap-2 border-2 p-3 rounded-2xl m-5 border-t-0">
-        <img
-          className="rounded-2xl h-[140px] object-contain"
-          src={imageUrl}
-          alt=""
-        />
-        <h4 className="font-bold md:h-[30px] text-base">{title}</h4>
-        <p className="h-[120px] overflow-hidden">{content}</p>
-        <div className="flex justify-between">
-          <Link to={`/add-news/${id}`}>
+      {data.map((item) => (
+        <article
+          key={item.id}
+          className="w-[300px] flex flex-col gap-2 border-2 p-3 rounded-2xl m-5 border-t-0"
+        >
+          <img
+            className="rounded-2xl h-[140px] object-cover"
+            src={item.image}
+            alt=""
+          />
+          <h4 className="font-bold md:h-[30px] text-base">{item.title}</h4>
+          <p className="h-[120px] overflow-hidden">{item.content}</p>
+          <div className="flex justify-between">
             <Button
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="bg-blue-600"
               name="Edit"
-              onClick={() => updateNews()}
+              onClick={() => {
+                setItems(item);
+                setEditForm(!editForm);
+              }}
             />
-          </Link>
-          <Button
-            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-            name="Delate"
-            onClick={() => deleteNews()}
+            <Button
+              className="bg-red-600"
+              name="Delate"
+              onClick={() => handleDelete(item.id)}
+            />
+          </div>
+        </article>
+      ))}
+
+      {editForm && (
+        <div className="w-full h-full right-0 top-0 fixed">
+          <button onClick={() => setEditForm(!editForm)} className="bg-[#fffffff6] w-full h-full -z-10 absolute"></button>
+          <UpdateNews
+            data={items}
+            handleClose={() => setEditForm(!editForm)}
+            setUpdateSuccessful={() => setUpdateSuccessful()}
           />
         </div>
-      </article>
+      )}
     </>
   );
 };
